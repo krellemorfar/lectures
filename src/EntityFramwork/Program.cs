1b2f41abc250f0cc4a79b1a778d5096d58e5431d
Kristoffer Winther Jessen <krelle@cnfa.dk>
2016-10-14 13:46:05 +0200

    EntityFramework solution created

--- src/EntityFramwork/Program.cs
+++ src/EntityFramwork/Program.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Lectures
+namespace EntityFramwork
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Select solution to see code for a specific subject");
         }
     }
 }
---
+++ src/OldschoolMysql/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OldschoolMysql
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is \"Oldschool\" MySql fetching using ADO.NET");
+
+            var connStr = "server=localhost;database=northwind;uid=admin;pwd=Fckfck123";
+ 
+            using (var connection = new MySqlConnection(connStr))
+            {
+                connection.Open();
+
+                var cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "SELECT * FROM category";
+
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        var id = rdr.GetInt32(0);
+                        var name = rdr.GetString(1);
+                        Console.WriteLine($"{id} {name}");
+                    }
+                }
+            }
+        }
+    }
+}
